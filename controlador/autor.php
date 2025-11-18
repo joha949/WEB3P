c3da13af1b1b539155d1f267f1a0b3c84ff6c7b9
joha949 <johvargassie@est.utn.ac.cr>
2025-11-18 13:27:30 -0600

    Refactor autor.php for improved structure and clarity

--- controlador/autor.php
+++ controlador/autor.php
@@ -2,7 +2,8 @@
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, cedula");
+
 
 
 // -----------------------------------------------------
@@ -141,4 +142,5 @@ switch ($op) {
         break;
 }
 ?>
+
 
